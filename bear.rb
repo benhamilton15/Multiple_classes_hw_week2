31b0875b14c6e13a0ee4469a8ca8d39eb8b76247
Ben Hamilton <ben.hamilton@me.com>
2019-04-10 10:00:21 +0100

    improved bear method for eating from river, changed 2 to number_of_fish

--- bear.rb
+++ bear.rb
@@ -5,16 +5,16 @@ attr_reader :name, :type
   def initialize(name, type)
     @name = name
     @type = type
-    @belly_of_fish = []
+    @belly_of_bear = []
   end
 
   def belly_count
-    return @belly_of_fish.length
+    return @belly_of_bear.length
   end
 
   def eat_fish(number_of_fish, river)
-    fish_eaten = river.fishes.pop(2)
-    @belly_of_fish += fish_eaten
+    fish_eaten = river.fishes.pop(number_of_fish)
+    @belly_of_bear += fish_eaten
   end
 
   def roar
